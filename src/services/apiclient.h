db711f42140ad01597105c6a7e09a84132200f20
BeketeLight <beketelight15@gmail.com>
2026-06-09 09:07:01 +0200

    apiClinet has been implemted. it will be refactore on whos to acces the token from the appSettings

--- src/services/apiclient.h
+++ src/services/apiclient.h
@@ -1,22 +1,82 @@
+
 #ifndef APICLIENT_H
 #define APICLIENT_H
 
 #include <QObject>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QJsonObject>
+#include <functional>
+#include <QMap>
 
-
-class ApiClient : public QObject
+class APIClient : public QObject
 {
     Q_OBJECT
+
 public:
-   explicit ApiClient(QObject *parent = nullptr);
-    void getRequest();
-    void postRequest();
-    void deleteRequest();
-    void putRequest();
-private:
+
+    using SuccessCallback =
+        std::function<void(bool success,
+                           const QJsonObject& response)>;
+
+    explicit APIClient(QObject *parent = nullptr);
+
+    void setBaseUrl(const QString& url);
+    void setAuthToken(const QString& token);
+
+    void get(
+        const QString& endpoint,
+        SuccessCallback callback
+        );
+
+    void post(
+        const QString& endpoint,
+        const QJsonObject& data,
+        SuccessCallback callback
+        );
+
+    void put(
+        const QString& endpoint,
+        const QJsonObject& data,
+        SuccessCallback callback
+        );
+
+    void del(
+        const QString& endpoint,
+        SuccessCallback callback
+        );
+
 signals:
-    void requestSuccess();
-    void requestFailed();
+
+    void networkError(const QString& message);
+    void authenticationRequired();
+
+private slots:
+
+    void onReplyFinished(QNetworkReply* reply);
+
+private:
+
+    struct PendingRequest
+    {
+        SuccessCallback callback;
+    };
+
+    QNetworkAccessManager* m_networkManager;
+
+    QString m_baseUrl;
+    QString m_authToken;
+
+    QMap<QNetworkReply*, PendingRequest> m_pendingRequests;
+
+    void sendRequest(
+        const QString& method,
+        const QString& endpoint,
+        const QJsonObject& data,
+        SuccessCallback callback
+        );
+
+    void setupHeaders(QNetworkRequest& request);
 };
 
-#endif // APICLIENT_H
+#endif
