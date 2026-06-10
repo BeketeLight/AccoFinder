90a08e94b1489dfdbcf0a286df6dba1d60f9c1d2
bryan-nathupo <bryannathupo@gmail.com>
2026-06-10 21:42:18 +0200

    implemented BookingRepository

--- src/repositories/impl/bookingrepositoryimpl.cpp
+++ src/repositories/impl/bookingrepositoryimpl.cpp
@@ -1,21 +1,160 @@
 #include "bookingrepositoryimpl.h"
+#include <QJsonObject>
+#include <QJsonValue>
+#include <QList>
+#include <QJsonArray>
+#include <QDateTime>
+#include "core/utils/appsettings.h"  //for persistence
+#include "services/apiclient.h"
 
-BookingRepositoryImpl::BookingRepositoryImpl()
+BookingRepositoryImpl::BookingRepositoryImpl(QObject *parent)
+    : IBookingRepository(parent)
 {
 
 }
 
-Booking *BookingRepositoryImpl::createBooking()
+void BookingRepositoryImpl::createBooking(
+    const QString& houseId,
+    const QDateTime& startDate,
+    const QDateTime& endDate,
+    const QString& specialNotes )
 {
+    QJsonObject payload;
+    payload["houseId"] = houseId;
+    payload["startDate"] = startDate.toString(Qt::ISODate);
+    payload["endDate"] = endDate.toString(Qt::ISODate);
+    if(!specialNotes.isEmpty()){
+        payload["specialNotes"] = specialNotes;
+    }
+
+    APIClient::instance().post(
+        "api/house-booking/",
+        payload,
+        [this](bool success,
+            const QJsonObject& response)
+        {
+            if(success){
+                Booking* booking = new Booking(
+                    response["id"].toString(),
+                    response["clientId"].toString(),
+                    response["roomId"].toString(),
+                    QDateTime::fromString(response["bookingDate"].toString(), Qt::ISODate),
+                    response["amount"].toDouble(),
+                    response["commissionAmount"].toDouble(),
+                    this
+                );
+                emit bookingCreated(booking);
+            }
+        });
+
 
 }
 
-Booking *BookingRepositoryImpl::getBooking()
+void BookingRepositoryImpl::getBooking()
 {
-
+    APIClient::instance().get(
+        "api/house-booking/",
+        [this] (bool success, 
+            const QJsonObject& response)
+           {
+             QList<Booking*> bookings;
+            if(success && response.contains("data")){
+                QJsonArray dataArray = response["data"].toArray();
+                for(const QJsonValue& value : dataArray){
+                    QJsonObject obj = value.toObject();
+                    Booking* booking = new Booking(
+                        obj["id"].toString(),
+                        obj["clientId"].toString(),
+                        obj["roomId"].toString(),
+                        QDateTime::fromString(obj["bookingDate"].toString(), Qt::ISODate),
+                        obj["amount"].toDouble(),
+                        obj["commissionAmount"].toDouble(),
+                        this
+                    );  
+                    bookings.append(booking);
+                } 
+            }
+            emit bookingsLoaded(bookings);
+        });
 }
 
-Booking *BookingRepositoryImpl::cancelBooking()
-{
+// void BookingRepositoryImpl::cancelBooking(const QString& id)
+// {
+//     APIClient::instance().patch(
+//         "api/house-booking/cancel/:" + id + "/cancel",
+//         [this] (bool success, const QJsonObject& response)
+//         {
+//             if(success){
+//                 Booking* booking = new Booking(
+//                     response["id"].toString(),
+//                     response["clientId"].toString(),
+//                     response["roomId"].toString(),
+//                     QDateTime::fromString(response["bookingDate"].toString(), Qt::ISODate),
+//                     response["amount"].toDouble(),
+//                     response["commissionAmount"].toDouble(),
+//                     this
+//                 );
+//                 emit bookingCancelled(booking);
+//             }
+//         }
+//     )
+// }
 
+void BookingRepositoryImpl::deleteBooking(const QString& id)
+{
+    APIClient::instance().del(
+        "api/house-booking/:" + id,
+        [this,id] (bool success, const QJsonObject& response)
+        {
+            if(success){
+                emit bookingDeleted(id);
+            }else
+            {
+                emit bookingError(response["error"].toString());
+            }   
+    });
+}
+// void BookingRepositoryImpl::confirmBooking(const QString& id)
+// {
+//     APIClient::instance().patch(
+//         "api/house-booking/:" + id + "/confirm",
+//         [this] (bool success, const QJsonObject& response)
+//         {
+//             if(success){
+//                 Booking* booking = new Booking(
+//                     response["id"].toString(),
+//                     response["clientId"].toString(),
+//                     response["roomId"].toString(),
+//                     QDateTime::fromString(response["bookingDate"].toString(), Qt::ISODate),
+//                     response["amount"].toDouble(),
+//                     response["commissionAmount"].toDouble(),
+//                     this
+//                 );
+//                 emit bookingConfirmed(booking);
+//             }
+//         }
+//     )
+// }
+void BookingRepositoryImpl::getBookingById(const QString& id)
+{
+    APIClient::instance().get(
+        "api/house-booking/:" + id,
+        [this] (bool success, const QJsonObject& response)
+        {
+        if(success){
+            Booking* booking = new Booking(
+            response["id"].toString(),
+            response["clientId"].toString(),
+            response["roomId"].toString(),
+            QDateTime::fromString(response["bookingDate"].toString(), Qt::ISODate),
+            response["amount"].toDouble(),
+            response["commissionAmount"].toDouble(),
+            this
+            );
+             emit bookingLoaded(booking);
+            }else
+            {
+                emit bookingError(response["error"].toString());
+            }
+     });
 }
--- src/repositories/interfaces/IBookingRepository.h
+++ src/repositories/interfaces/IBookingRepository.h
@@ -10,15 +10,19 @@ class IBookingRepository : public QObject
 public:
     explicit IBookingRepository(QObject *parent = nullptr)
         : QObject(parent) {}
-    virtual Booking* createBooking() = 0;
-    virtual Booking* getBooking() = 0;
-    virtual Booking* cancelBooking() = 0;
+    virtual void createBooking(
+        const QString& houseId,
+        const QDateTime& startDate,
+        const QDateTime& endDate,
+        const QString& specialNotes ) = 0;
+
+    virtual void getBooking() = 0;
+    virtual void getBookingById(const QString& id) = 0;
+    // virtual void cancelBooking(const QString& id) = 0;
+    // virtual void confirmBooking(const QString& id) = 0;
+    virtual void deleteBooking(const QString& id) = 0;
 
     virtual ~IBookingRepository() {}
-private:
-
-signals:
-
 };
 
 
