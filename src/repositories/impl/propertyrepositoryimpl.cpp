fb58ece29b51edcc5e294e8a01b79d1d3b1f6641
bryan-nathupo <bryannathupo@gmail.com>
2026-06-11 14:42:40 +0200

    Implemented propertyRepository #17

--- src/repositories/impl/propertyrepositoryimpl.cpp
+++ src/repositories/impl/propertyrepositoryimpl.cpp
@@ -1,24 +1,97 @@
 #include "propertyrepositoryimpl.h"
+#include <QJsonObject>
+#include <QJsonArray>
+#include "services/apiclient.h"
+#include <QList>
 
-
-PropertyRepositoryImpl::PropertyRepositoryImpl()
+PropertyRepositoryImpl::PropertyRepositoryImpl(QObject *parent)
+    : IPropertyRepository(parent)
 {
 
 }
 
-Property *PropertyRepositoryImpl::getProperties()
+QJsonObject PropertyRepositoryImpl::propertyDtoToJson(const PropertyDto& dto ) const
 {
+    QJsonObject json;
+    json["id"] = dto.id;
+    json["title"] = dto.title;
+    json["location"] = dto.location;
+    json["price"] = dto.price;
+    json["description"] = dto.description;
+    json["status"] = dto.status;
+    json["agentId"] = dto.agentId;
+    json["landlordId"] = dto.landlordId;
+    json["createdAt"]  = dto.createdAt.toString(Qt::ISODate);
+    
+    return json;
 
 }
 
-Property *PropertyRepositoryImpl::getPropertyById()
+PropertyDto PropertyRepositoryImpl::jsonToPropertyDto(const QJsonObject& json) const
 {
+    return PropertyDto::fromJson(json);
+}
 
+Property* PropertyRepositoryImpl::dtoToDomainModel(const PropertyDto& dto) const
+{
+    return dto.toDomainModel();
+}
+
+void PropertyRepositoryImpl::getProperties()
+{
+    APIClient::instance().get(
+        "api/house-listing/",
+        [this] (bool success, const QJsonObject& response )
+        {
+            QList<Property*> properties;
+            if(success && response.contains("data")){
+                QJsonArray dataArray = response["data"].toArray();
+                for(const QJsonValue& value: dataArray){
+                    PropertyDto dto = jsonToPropertyDto(value.toObject());
+                    properties.append(dtoToDomainModel(dto));
+                }
+                emit propertiesLoaded(properties);
+            }
+          
+        }
+    );
+
+}
+
+void PropertyRepositoryImpl::getPropertyById(const QString& houseId)
+{
+    APIClient::instance().get(
+        "api/house-listing/" + houseId,
+        [this] (bool success, const QJsonObject& response)
+        {
+            if(success){
+                PropertyDto dto = jsonToPropertyDto(response);
+                Property* property = dtoToDomainModel(dto);
+
+                emit propertyLoaded(property);
+            }
+           
+        }
+    );
     
 }
 
-Property *PropertyRepositoryImpl::updateProperty()
+void PropertyRepositoryImpl::updateProperty(const QString& houseId, const PropertyDto& dto)
 {
+    QJsonObject payload = propertyDtoToJson(dto);
+    APIClient::instance().put(
+        "api/house-listing/:" + houseId, 
+        payload,
+        [this] (bool success, const QJsonObject& response)
+        {
+            if(success){
+                PropertyDto updatedDto = jsonToPropertyDto(response);
+                Property* property = dtoToDomainModel(updatedDto);
+                emit propertyUpdated(property);
+            }
+          
+        }
+    );
 
 }
 
--- src/repositories/impl/propertyrepositoryimpl.h
+++ src/repositories/impl/propertyrepositoryimpl.h
@@ -1,17 +1,31 @@
 #ifndef PROPERTYREPOSITORYIMPL_H
 #define PROPERTYREPOSITORYIMPL_H
 
+#include "application/dto/propertydto.h"
 #include "repositories/interfaces/IPropertyRepository.h"
 #include "models/property.h"
 
 class PropertyRepositoryImpl : public IPropertyRepository
 {
+    Q_OBJECT
 public:
-    PropertyRepositoryImpl();
-    Property* getProperties() override;
-    Property* getPropertyById() override;
-    Property* updateProperty() override;
+    explicit PropertyRepositoryImpl(QObject *parent = nullptr);
+    void getProperties() override;
+    void getPropertyById(const QString& houseId) override;
+    void updateProperty(const QString& houseId, const PropertyDto& dto) override;
+
 private:
+//    ===== HELPER METHODS FOR DTO, JSON CONVERSION ====
+QJsonObject propertyDtoToJson(const PropertyDto& dto ) const;
+PropertyDto jsonToPropertyDto(const QJsonObject& json) const;
+Property* dtoToDomainModel(const PropertyDto& dto) const;
+
+signals:
+    void propertyLoaded(Property* property);
+    void propertiesLoaded(const QList<Property*>& properties);
+    void propertyUpdated(Property* properties);
+    void propertyError(const QString& error);
+
 };
 
 #endif // PROPERTYREPOSITORYIMPL_H
