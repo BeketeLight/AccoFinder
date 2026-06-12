8c63b80a16226131d001e08631c75f793de290cf
bryan-nathupo <bryannathupo@gmail.com>
2026-06-12 15:25:03 +0200

    implemented VerificationRepository #50

--- src/repositories/impl/verificationrepositoryimpl.cpp
+++ src/repositories/impl/verificationrepositoryimpl.cpp
@@ -1,3 +1,112 @@
 #include "verificationrepositoryimpl.h"
+#include <QJsonObject>
+#include <QJsonArray>
+#include "services/apiclient.h"
+#include <QList>
 
-VerificationRepositoryImpl::VerificationRepositoryImpl() {}
+
+VerificationRepositoryImpl::VerificationRepositoryImpl(QObject *parent) 
+    : IVerificationRepository(parent)
+{
+
+}
+
+void VerificationRepositoryImpl::createVerification(const QString& id,
+                                                   const QString& agentId, 
+                                                   const QString& propertyId,
+                                                   const QString& notes, 
+                                                   const QDateTime& verifiedAt,
+                                                   VerificationStatus& status)
+{
+    VerificationDto dto(id,agentId,propertyId,notes,verifiedAt,status);
+
+    APIClient::instance().post(
+        "api/property/:" + id,
+        dto.toJson(),
+        [this] (bool success, const QJsonObject& response)
+        {
+            if(success){
+                VerificationDto updateDto = VerificationDto::fromJson(response["data"].toObject());
+                Verification* verification = updateDto.toDomainModel();
+
+                emit propertyCreated(verification);
+            }else{
+                emit propertyError(response["error"].toString());
+            }
+        }
+    );
+}                                                    
+    
+
+void VerificationRepositoryImpl::getVerificationHistory()
+{
+    APIClient::instance().get(
+        "/api/property/history",
+        [this]  (bool success, const QJsonObject& response)  
+        {   QList<Verification*> verifications;
+            if(success && response.contains("data")){
+              QJsonArray dataArray = response["data"].toArray();
+              for(const QJsonValue& value: std::as_const(dataArray)){
+                VerificationDto dto = VerificationDto::fromJson(value.toObject());
+                verifications.append(dto.toDomainModel());
+              }
+              emit propertyHistoryLoaded(verifications);
+            }else{
+                emit propertyError(response["error"].toString());
+            }
+        }
+    );
+}
+
+void VerificationRepositoryImpl::approveProperty(const QString& id, 
+                                                const QString& agentId, 
+                                                const QString& propertyId, 
+                                                const QString& notes, 
+                                                const QDateTime& verifiedAt, 
+                                                VerificationStatus& status)
+{
+    VerificationDto dto(id,agentId,propertyId,notes,verifiedAt,status);
+
+    APIClient::instance().patch(
+        "api/property/:" + propertyId,
+        dto.toJson(),
+        [this] (bool success, const QJsonObject& response)
+        {
+            if(success){
+                VerificationDto updatedADto = VerificationDto::fromJson(response["data"].toObject());
+                Verification* verification = updatedADto.toDomainModel();
+                
+                emit propertyApproved(verification);
+            }else{
+                emit propertyError(response["error"].toString());
+            }
+        }
+    );
+
+}
+void VerificationRepositoryImpl::rejectProperty(const QString& id, 
+                                                const QString& agentId, 
+                                                const QString& propertyId, 
+                                                const QString& notes, 
+                                                const QDateTime& verifiedAt, 
+                                                VerificationStatus& status)
+{
+    VerificationDto dto(id,agentId,propertyId,notes,verifiedAt,status);
+
+    APIClient::instance().patch(
+        "api/property:" + propertyId,
+        dto.toJson(),
+        [this] (bool success,const QJsonObject& response)
+        {
+            if(success){
+                VerificationDto updatedDto = VerificationDto::fromJson(response["data"].toObject());
+                Verification* verification = updatedDto.toDomainModel();
+
+                emit propertyRejected(verification);
+            }else{
+                emit propertyError(response["error"].toString());
+            }
+        }
+    );
+
+}
--- src/repositories/impl/verificationrepositoryimpl.h
+++ src/repositories/impl/verificationrepositoryimpl.h
@@ -1,10 +1,45 @@
 #ifndef VERIFICATIONREPOSITORYIMPL_H
 #define VERIFICATIONREPOSITORYIMPL_H
 
-class VerificationRepositoryImpl
+#include "repositories/interfaces/IVerificationRepository.h"
+#include "application/dto/verificationdto.h"
+#include "models/verification.h"
+
+class VerificationRepositoryImpl : public IVerificationRepository
 {
+    Q_OBJECT
 public:
-    VerificationRepositoryImpl();
+   explicit VerificationRepositoryImpl(QObject* parent = nullptr);
+
+   void createVerification(const QString& id, 
+                           const QString& agentId, 
+                           const QString& propertyId, 
+                           const QString& notes, 
+                           const QDateTime& verifiedAt,
+                           VerificationStatus& status) override;
+
+   void getVerificationHistory() override;
+
+   void approveProperty(const QString& id,
+                        const QString& propertyId, 
+                        const QString& agentId, 
+                        const QString& notes, 
+                        const QDateTime& verifiedAt, 
+                        VerificationStatus& status) override;
+
+   void rejectProperty(const QString& id,
+                       const QString& propertyId, 
+                       const QString& agentId, 
+                       const QString& notes, 
+                       const QDateTime& verifiedAt, 
+                       VerificationStatus& status) override;
+
+signals:
+    void propertyCreated(Verification* verification);
+    void propertyRejected(Verification* verification);
+    void propertyHistoryLoaded(const QList<Verification*>& verfications);
+    void propertyApproved(Verification* verification) ;
+    void propertyError(const QString& error);
 };
 
 #endif // VERIFICATIONREPOSITORYIMPL_H
