59991c090e668366046082b236b85d3507180f47
lcfranklin <franklinlungu7@gmail.com>
2026-06-12 21:47:14 +0200

    propertyController Implementation

--- src/application/controllers/propertycontroller.cpp
+++ src/application/controllers/propertycontroller.cpp
@@ -1,18 +1,58 @@
 #include "propertycontroller.h"
 
-PropertyController::PropertyController() {}
-
-Property *PropertyController::getProperties()
+PropertyController::PropertyController(QObject *parent)
+    : QObject(parent),
+      m_propertyRepositoryImpl(new PropertyRepositoryImpl(this))
 {
-    
+    connect(m_propertyRepositoryImpl, &PropertyRepositoryImpl::propertiesLoaded,
+            this, &PropertyController::propertiesLoaded);
+
+    connect(m_propertyRepositoryImpl, &PropertyRepositoryImpl::propertyLoaded,
+            this, &PropertyController::propertyLoaded);
+
+    connect(m_propertyRepositoryImpl, &PropertyRepositoryImpl::propertyUpdated,
+            this, &PropertyController::propertyUpdated);
+
+    connect(m_propertyRepositoryImpl, &PropertyRepositoryImpl::propertyError,
+            this, &PropertyController::propertyError);
 }
 
-Property *PropertyController::getPropertyById()
+void PropertyController::getProperties()
 {
-    
+    m_propertyRepositoryImpl->getProperties();
 }
 
-Property *PropertyController::updateProperty()
+void PropertyController::getPropertyById(const QString& houseId)
 {
-    
+    if (houseId.isEmpty()) {
+        emit propertyError("houseId cannot be empty");
+        return;
+    }
+    m_propertyRepositoryImpl->getPropertyById(houseId);
+}
+
+void PropertyController::updateProperty(const QString& houseId,
+                                        const QString& title,
+                                        const QString& description,
+                                        double price,
+                                        const QString& costCategory)
+{
+    if (houseId.isEmpty()) {
+        emit propertyError("houseId cannot be empty");
+        return;
+    }
+    if (title.isEmpty()) {
+        emit propertyError("title cannot be empty");
+        return;
+    }
+    if (price <= 0) {
+        emit propertyError("price must be greater than 0");
+        return;
+    }
+    if (costCategory != "Low_Cost" && costCategory != "Medium_Cost" && costCategory != "High_Cost") {
+        emit propertyError("invalid costCategory");
+        return;
+    }
+
+    m_propertyRepositoryImpl->updateProperty(houseId, title, description, price, costCategory);
 }
--- src/application/controllers/propertycontroller.h
+++ src/application/controllers/propertycontroller.h
@@ -2,6 +2,7 @@
 #define PROPERTYCONTROLLER_H
 
 #include <QObject>
+#include <QList>
 #include "models/property.h"
 #include "repositories/impl/propertyrepositoryimpl.h"
 
@@ -9,12 +10,24 @@ class PropertyController : public QObject
 {
     Q_OBJECT
 public:
-    PropertyController();
-    Property* getProperties();
-    Property* getPropertyById();
-    Property* updateProperty();
+    explicit PropertyController(QObject *parent = nullptr);
+
+    Q_INVOKABLE void getProperties();
+    Q_INVOKABLE void getPropertyById(const QString& houseId);
+    Q_INVOKABLE void updateProperty(const QString& houseId,
+                                    const QString& title,
+                                    const QString& description,
+                                    double price,
+                                   const QString& costCategory);
+
+signals:
+    void propertiesLoaded(const QList<Property*>& properties);
+    void propertyLoaded(Property* property);
+    void propertyUpdated(Property* property);
+    void propertyError(const QString& error);
+
 private:
-    PropertyRepositoryImpl *m_propertyRepositoryImpl = new PropertyRepositoryImpl();
+    PropertyRepositoryImpl *m_propertyRepositoryImpl;
 };
 
 #endif // PROPERTYCONTROLLER_H
