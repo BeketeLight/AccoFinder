1d0de187cc9d0496e105fbcfacae9866c47a473d
BeketeLight <beketelight15@gmail.com>
2026-06-14 15:52:15 +0200

    dispute controller implematatio completed

--- src/application/controllers/disputecontroller.cpp
+++ src/application/controllers/disputecontroller.cpp
@@ -2,4 +2,30 @@
 
 DisputeController::DisputeController(QObject *parent)
     : QObject{parent}
-{}
+{
+    connect(m_disputeRepository,&DisputeRepositoryImpl::disputesLoaded,
+            this,&DisputeController::disputesLoaded);
+    connect(m_disputeRepository,&DisputeRepositoryImpl::disputeRaised,
+            this,&DisputeController::disputeRaised);
+    connect(m_disputeRepository,&DisputeRepositoryImpl::disputeResolved,
+            this,&DisputeController::disputeResolved);
+}
+
+void DisputeController::raiseDispute(const QString& raisedBy,
+                                     const QString& title,
+                                     const QString& status,
+                                     const QString& description)
+{
+    m_disputeRepository->raiseDispute(raisedBy,title,status,description);
+}
+
+void DisputeController::resolveDispute(const QString& disputeId)
+{
+    m_disputeRepository->resolveDispute(disputeId);
+}
+
+void DisputeController::getDisputes()
+{
+    m_disputeRepository->getDisputes();
+
+}
--- src/application/controllers/disputecontroller.h
+++ src/application/controllers/disputecontroller.h
@@ -1,6 +1,7 @@
 #ifndef DISPUTECONTROLLER_H
 #define DISPUTECONTROLLER_H
 
+#include <QList>
 #include <QObject>
 #include "models/dispute.h"
 #include "repositories/impl/disputerepositoryimpl.h"
@@ -10,10 +11,19 @@ class DisputeController : public QObject
     Q_OBJECT
 public:
     explicit DisputeController(QObject *parent = nullptr);
-    Dispute* raiseDispute();
-    Dispute* resolveDispute();
+    void raiseDispute(const QString& raisedBy,
+                      const QString& title,
+                      const QString& status,
+                      const QString& description);
+    void resolveDispute(const QString& disputeId);
     void getDisputes();
+private:
+    DisputeRepositoryImpl* m_disputeRepository = nullptr;
+    QList<Dispute*> m_disputes;
 signals:
+    void disputeRaised(Dispute* dispute);
+    void disputeResolved(Dispute* dispute);
+    void disputesLoaded(QList<Dispute*> disputes);
 };
 
 #endif // DISPUTECONTROLLER_H
