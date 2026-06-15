79b4b17c10b1b8a4850df04aae56da5a4e7ef0eb
bryan-nathupo <bryannathupo@gmail.com>
2026-06-15 11:46:18 +0200

    Implemented ReviewListModel

--- src/presentation/models/reviewlistmodel.cpp
+++ src/presentation/models/reviewlistmodel.cpp
@@ -4,26 +4,85 @@ ReviewListModel::ReviewListModel(QObject *parent)
     : QAbstractListModel(parent)
 {}
 
-QVariant ReviewListModel::headerData(int section, Qt::Orientation orientation, int role) const
-{
-    // FIXME: Implement me!
-}
+ReviewListModel::~ReviewListModel()
+{}
 
 int ReviewListModel::rowCount(const QModelIndex &parent) const
 {
     // For list models only the root node (an invalid parent) should return the list's size. For all
     // other (valid) parents, rowCount() should return 0 so that it does not become a tree model.
-    if (parent.isValid())
-        return 0;
+  Q_UNUSED(parent);
+  return m_reviews.count();
 
     // FIXME: Implement me!
+}
+QVariant ReviewListModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    // FIXME: Implement me!
+    Q_UNUSED(section);
+    Q_UNUSED(orientation);
+    Q_UNUSED(role);
+    return QVariant();
+}
+
+QHash<int, QByteArray> ReviewListModel::roleNames() const
+{
+    static QHash<int, QByteArray> roles;
+    roles[IdRole] = "reviewId";
+    roles[RatingRole] = "rating";
+    roles[CommentRole] = "comment";
+    roles[CreatedAtRole] = "comment";
+    return roles;
 }
 
 QVariant ReviewListModel::data(const QModelIndex &index, int role) const
 {
-    if (!index.isValid())
+    if (!index.isValid() || index.row() < 0 || index.row() >= m_reviews.size()){
         return QVariant();
+    }
+    
+    Review* review = m_reviews.at(index.row()).data();
+        // return QVariant();
+    
+    switch(role)
+    {
+    case IdRole:
+        return review->getId();
+    case RatingRole:
+        return review->getRating();
+    case CommentRole:
+        return review->getComment();
+    case CreatedAtRole:
+        return review->getCreatedAt().toString("dd/MM/yyyy");        
 
+    } 
     // FIXME: Implement me!
     return QVariant();
+}
+
+void ReviewListModel::addReview(Review* review)
+{
+    if(!review)
+        return;
+    
+    beginInsertRows(QModelIndex(), m_reviews.size(), m_reviews.count());
+    m_reviews.append(QSharedPointer<Review>(review));
+    endInsertRows();
+
+    emit countChanged(m_reviews.size());
+    emit reviewSubmitted(review);
+    
+    qDebug() << "Review added";// for checking execution
+    
+}
+void ReviewListModel::removeReview(int index)
+{
+    if(index < 0 || index >= m_reviews.size()){
+        return;
+    }
+    beginRemoveRows(QModelIndex(), index, index);
+    m_reviews.removeAt(index);
+    endRemoveRows();
+
+    emit countChanged(m_reviews.count());
 }
--- src/presentation/models/reviewlistmodel.h
+++ src/presentation/models/reviewlistmodel.h
@@ -2,13 +2,26 @@
 #define REVIEWLISTMODEL_H
 
 #include <QAbstractListModel>
+#include <QHash>
+#include <QList>
+#include <QByteArray>
+#include <QVector>
+#include "models/review.h"
 
 class ReviewListModel : public QAbstractListModel
 {
     Q_OBJECT
-
+    //custom rows for QML access
 public:
+    enum reviewRoles{
+        IdRole = Qt::UserRole,
+        RatingRole,
+        CommentRole,
+        CreatedAtRole,
+    };
+
     explicit ReviewListModel(QObject *parent = nullptr);
+    ~ReviewListModel();
 
     // Header:
     QVariant headerData(int section,
@@ -19,8 +32,19 @@ public:
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
 
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QHash<int, QByteArray> roleNames() const override;
+
+    // === INVOCABLE METHODS FOR QML
+    Q_INVOKABLE void addReview(Review* review);
+    Q_INVOKABLE void removeReview(int index);
+   
+
+signals:
+    void countChanged(int newcount);
+    void reviewSubmitted(Review* review);
 
 private:
+    QVector<QSharedPointer<Review>> m_reviews; //storing pointers to review objetct
 };
 
 #endif // REVIEWLISTMODEL_H
