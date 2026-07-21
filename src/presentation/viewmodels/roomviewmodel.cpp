04738e14ade08159b0f130e661d4e2acee4b398c
BeketeLight <beketelight15@gmail.com>
2026-07-21 23:26:17 +0200

    room refactoring started

--- src/presentation/viewmodels/roomviewmodel.cpp
+++ src/presentation/viewmodels/roomviewmodel.cpp
@@ -1,40 +1,40 @@
-// #include "roomviewmodel.h"
+#include "roomviewmodel.h"
 
-// RoomViewModel::RoomViewModel(QObject *parent)
-//     :QObject(parent)
-// {
-//     connect(m_roomController.data(),&RoomController::roomCreated,
-//             this,&RoomViewModel::onRoomCreated);
-//     connect(m_roomController.data(),&RoomController::roomLoaded,
-//             this,&RoomViewModel::onRoomLoaded);
-//     connect(m_roomController.data(),&RoomController::roomsLoaded,
-//             this,&RoomViewModel::onRoomsLoaded);
-// }
+RoomViewModel::RoomViewModel(QObject *parent)
+    :QObject(parent)
+{
+    connect(m_roomController.data(),&RoomController::roomCreated,
+            this,&RoomViewModel::onRoomCreated);
+    connect(m_roomController.data(),&RoomController::roomLoaded,
+            this,&RoomViewModel::onRoomLoaded);
+    connect(m_roomController.data(),&RoomController::roomsLoaded,
+            this,&RoomViewModel::onRoomsLoaded);
+}
 
-// void RoomViewModel::onRoomsLoaded(const QList<QSharedPointer<Room> > &rooms)
-// {
-//     if(m_roomListModel)
-//     {
-//         m_roomListModel->setRooms(rooms);
-//     }
+void RoomViewModel::onRoomsLoaded(const QList<QSharedPointer<Room> > &rooms)
+{
+    if(m_roomListModel)
+    {
+        m_roomListModel->setRooms(rooms);
+    }
 
-// }
+}
 
-// void RoomViewModel::onRoomLoaded(const QSharedPointer<Room>& room)
-// {
-//     if(m_roomListModel)
-//     {
-//         m_roomListModel->apppendRoom(room);
-//     }
+void RoomViewModel::onRoomLoaded(const QSharedPointer<Room>& room)
+{
+    if(m_roomListModel)
+    {
+        m_roomListModel->apppendRoom(room);
+    }
 
-// }
+}
 
-// void RoomViewModel::onRoomCreated(const QSharedPointer<Room> &room)
-// {
+void RoomViewModel::onRoomCreated(const QSharedPointer<Room> &room)
+{
     
-// }
+}
 
-// void RoomViewModel::onError(const QString &message)
-// {
+void RoomViewModel::onError(const QString &message)
+{
     
-// }
+}
