eed49c6d01c02cc7a32bb00bad217cce9a8af233
CassimJT <cassimjaffer47@gmail.com>
2026-08-22 09:23:38 +0200

    added run time camera permission

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <QCoreApplication>
 #include <QQuickStyle>
 #include "src/core/utils/appsettings.h"
+#include "src/core/utils/apppermission.h"
 #include "src/application/controllers/authcontroller.h"
 #include <QQmlContext>
 #include <QTimer>
@@ -31,10 +32,13 @@ int main(int argc, char *argv[])
     QQmlApplicationEngine engine;
     //Appsettings
     AppSettings& appSettings = AppSettings::instance();
+    //AppPermission
+    AppPermission& appPermission = AppPermission::instance();
     //Auth Controller
     AuthController authController;
     //ContextProperty
     engine.rootContext()->setContextProperty("AppSettings", &appSettings);
+    engine.rootContext()->setContextProperty("AppPermission", &appPermission);
     engine.rootContext()->setContextProperty("AuthController", &authController);
     QObject::connect(
         &engine,
